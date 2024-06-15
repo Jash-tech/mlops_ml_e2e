7c54225cfb049e5028b1dd4bf7ae33543f5a5bf1
Jash-tech <jashsuke@gmail.com>
2024-06-15 18:24:37 +0530

    dvc initialized and .yaml created

--- src/pipelines/training.py
+++ src/pipelines/training.py
@@ -29,41 +29,58 @@ from src.components.model_monitoring import ModelEvaluation
 class Training:
     def start_data_ingestion(self):
         try:
+            logging.info("Starting data ingestion...")
             data_ingestion=DataIngestion()
             train_data_path,test_data_path=data_ingestion.initiate_dataingestion()
+            logging.info("Completed data ingestion")
             return train_data_path,test_data_path
         except Exception as e:
+            logging.info("Error in data Ingestion")
             raise CustomException(e,sys)
         
 
     def start_data_transformation(self,train_data_path,test_data_path):
         try:
+            logging.info("Starting data Transformation...")
             data_transformation=DataTransformation()
             train_arr,test_arr=data_transformation.initiate_datatransformation(train_data_path,test_data_path)
+            logging.info("Completed data transformation...")
             return train_arr,test_arr
         
         except Exception as e:
+            logging.info("Error in Data transformation...")
             raise CustomException(e,sys)
         
     def start_model_training(self,train_arr,test_arr):
         try:
+            logging.info("Starting model training...")
             model_trainer=ModelTrainer()
             model_trainer.initiate_model_training(train_arr,test_arr)
             
         except Exception as e:
+            logging.info("Error in model training...")
             raise CustomException(e,sys)
         
     def start_training(self):
         try:
+            logging.info("Compiling all components")
             train_data_path,test_data_path=self.start_data_ingestion()
             train_arr,test_arr=self.start_data_transformation(train_data_path,test_data_path)
             self.start_model_training(train_arr,test_arr)
+            logging.info("Completed...")
 
         except Exception as e:
+            logging.info("Error in Model overall")
             raise CustomException(e,sys)
             
 
-        
+
+# if __name__ == "__main__":
+#     try:
+#         training_obj = Training()
+#         training_obj.start_training()
+#     except Exception as e:
+#         raise CustomException(e,sys)
 
 
 
